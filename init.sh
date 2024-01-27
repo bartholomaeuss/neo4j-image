be157a0b2cf026c2141a1318672d4454d4e36a70
bart <bartholomaeus.s@googlemail.com>
2024-01-27 16:46:11 +0100

    :tada:

---
+++ init.sh
@@ -0,0 +1,7 @@
+docker run \
+    --publish=7474:7474 --publish=7687:7687 \
+    --volume=$HOME/neo4j/data:/data \
+    neo4j
+
+
+docker run -d --publish=7474:7474 --publish=7687:7687 --volume=$HOME/neo4j/data:/data neo4j
